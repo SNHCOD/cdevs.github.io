067fd03334442d5d2eecadb97185520e35fb184a
Samuel Nocelo Hernandez <samuel@finalsoft.com.mx>
2023-01-26 02:26:08 -0600

    first

---
+++ _next/static/X89IEORYON-nJAQLA5Zmv/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/814-f83b2571d190bfbd.js","static/chunks/pages/index-6db9c3c7a3ae9d1a.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
